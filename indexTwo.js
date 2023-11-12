60b804b1a4aae384602748aba66040e7cc935a2b
Shiham123 <s36651786@gmail.com>
2023-11-12 19:05:15 +0600

    mongoose set as i wanted

--- indexTwo.js
+++ indexTwo.js
@@ -11,24 +11,16 @@ app.use(express.json());
 const port = 5000;
 const uri = 'mongodb://localhost:27017';
 
+let menuCollection;
+let testimonialCollection;
+
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
 
     const bistroDatabase = mongoose.connection.useDb('bistroDB');
-    const menuCollection = bistroDatabase.collection('menuItem');
-    const testimonialCollection = bistroDatabase.collection('testimonial');
-
-    app.get('/menu', async (request, response) => {
-      const cursor = menuCollection.find();
-      const result = await cursor.toArray();
-      response.status(200).send(result);
-    });
-
-    app.get('/reviews', async (request, response) => {
-      const cursor = await testimonialCollection.find().toArray();
-      response.status(200).send(cursor);
-    });
+    menuCollection = bistroDatabase.collection('menuItem');
+    testimonialCollection = bistroDatabase.collection('testimonial');
 
     console.log('MongoDB is connected');
   } catch (error) {
@@ -36,6 +28,27 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+app.get('/menu', async (request, response) => {
+  try {
+    const cursor = menuCollection.find();
+    const result = await cursor.toArray();
+    response.status(200).send(result);
+  } catch (error) {
+    console.error('Error fetching menu items:', error);
+    response.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/reviews', async (request, response) => {
+  try {
+    const cursor = await testimonialCollection.find().toArray();
+    response.status(200).send(cursor);
+  } catch (error) {
+    console.error('Error fetching testimonials:', error);
+    response.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.get('/', async (request, response) => {
   response.send('successfully');
